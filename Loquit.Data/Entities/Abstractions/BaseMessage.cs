d770e8c24493ee2c066b7d9fe0b7fcc2928eb062
Palatacemy <g.s.tsonev@gmail.com>
2024-04-10 22:01:58 +0300

    Fixes

--- Loquit.Data/Entities/Abstractions/BaseMessage.cs
+++ Loquit.Data/Entities/Abstractions/BaseMessage.cs
@@ -8,12 +8,7 @@ namespace Loquit.Data.Entities.Abstractions
 {
     public abstract class BaseMessage : BaseEntity
     {
-        public BaseMessage()
-        {
-            IsEdited = false;
-        }
         public DateTime TimeOfSending { get; set; }
         public string SenderUserId { get; set; }
-        public bool IsEdited { get; set; }
     }
 }
